1ddeea5a50fc22680dccacf88cf2b133de3e7548
unknown <yuri_salgadinh@hotmail.com>
2021-03-28 17:35:54 -0300

    add serviceWorker manual for pwa, react template missing sw

---
+++ public/worker.js
@@ -0,0 +1,44 @@
+var CACHE_NAME = 'covid19-pwa';
+var urlsToCache = [
+    '/'
+];
+
+// install worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// cache and request
+self.addEventListener('fetch', event => {
+    event.responseWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// update worker
+self.addEventListener('activate', event => {
+    var cacheWhitelist = ['pwa-task-manager'];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheName.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        })
+    )
+})

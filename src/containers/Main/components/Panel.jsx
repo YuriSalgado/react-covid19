cd8d35197c38b7d3c18a8b9a7e05d353b771f0ab
unknown <yuri_salgadinh@hotmail.com>
2021-03-28 16:48:32 -0300

    Aula 7, app on, go to pwa

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -19,6 +19,36 @@ function Panel({ updateAt, onChange, data, country, getCovidData}) {
             </MenuItem>
         )
     }
+
+    const textCovid19 = `País: ${country}`
+
+    const copyInfo = () => {
+        navigator.clipboard.writeText(textCovid19)
+    }
+
+    const shareInfo = () => {
+        navigator.share({
+            title: `Dados do Covid19 - ${country}`,
+            text: textCovid19,
+            url: 'https://covid19dio.netfly.app/'
+        })
+    }
+
+    const renderShareButton = (
+         <div>
+             <Button variant="contained" color="primary" onClick={shareInfo}>
+                 Compartilhar
+             </Button>
+         </div>
+    )
+
+    const renderCopyButton = (
+        <div>
+            <Button variant="contained" color="primary" onClick={copyInfo}>
+                Copiar
+            </Button>
+        </div>
+    )
     
     return (
         <Card>
@@ -33,6 +63,7 @@ function Panel({ updateAt, onChange, data, country, getCovidData}) {
                         </Select>
                     </div>
                 </div>
+                { navigationHasShare ? renderShareButton : renderCopyButton }
             </CardPanelContentStyled>
         </Card>
     )
